7842e885b266c09f21d8ef3fb81ec9e6861c8750
unniiiverse <unniiiverse@gmail.com>
2022-09-07 17:56:05 +0300

    uvst 1.1.3 add: picture tag wrapper for img

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,12 @@
-import fileinclude from "gulp-file-include"
+import fileinclude from "gulp-file-include";
+import webpHtmlNosvg from 'gulp-webp-html-nosvg';
 
 export const html = () => {
     return app.gulp.src(app.path.src.html)
         .pipe(fileinclude({
             prefix: '@@'
         }))
-        .pipe(app.plugins.replace(/@img\//g, './img/'))
+        .pipe(app.plugins.replace(/@img\//g, 'img/'))
+        .pipe(webpHtmlNosvg())
         .pipe(app.gulp.dest(app.path.build.html));
 }
