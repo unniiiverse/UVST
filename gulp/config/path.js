f50dc4b8eab7efa871869c5a0d81f3e97473f62f
unniiiverse <unniiiverse@gmail.com>
2022-09-07 16:09:27 +0300

    uvst 1.1.3 add: html modules

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,12 +7,15 @@ const srcFolder = `./src`;
 export const path = {
     build: {
         files: `${buildFolder}/`,
+        html: `${buildFolder}/`,
     },
     src: {
         files: `${srcFolder}/**/*.*`,
+        html: `${srcFolder}/*.html`,
     },
     watch: {
         files: `${srcFolder}/**/*.*`,
+        html: `${srcFolder}/**/*.html`,
     },
     clean: buildFolder,
     srcFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,14 @@ global.app = {
 
 import { copy } from './gulp/tasks/copy.js';
 import { clear } from './gulp/tasks/clear.js';
+import { html } from './gulp/tasks/html.js';
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
 }
 
-const dev = gulp.series(clear, copy, watcher);
+const tasks = gulp.parallel(copy, html)
+const dev = gulp.series(clear, tasks, watcher);
 
 gulp.task('default', dev);
