ffec8b4dbb8d5064c609b419fcc711765609ec63
unniiiverse <unniiiverse@gmail.com>
2022-09-07 18:07:44 +0300

    uvst 1.1.3 add: nocache key for src in css and js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,5 +1,6 @@
 import fileinclude from "gulp-file-include";
 import webpHtmlNosvg from 'gulp-webp-html-nosvg';
+import nocache from 'gulp-version-number';
 
 export const html = () => {
     return app.gulp.src(app.path.src.html)
@@ -8,5 +9,12 @@ export const html = () => {
         }))
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
         .pipe(webpHtmlNosvg())
+        .pipe(nocache({
+            'value': '%DT%',
+            'append': {
+                'key': 'nocache',
+                'to': ['css', 'js']
+            }
+        }))
         .pipe(app.gulp.dest(app.path.build.html));
 }
