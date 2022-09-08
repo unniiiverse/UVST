f5da71d55194fa41931b6242a2fc606194c236ab
unniiiverse <unniiiverse@gmail.com>
2022-09-08 18:39:50 +0300

    uvst 1.1.3 add: auto prefixer, css compressor

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,6 +1,10 @@
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
+import cleanCss from 'gulp-clean-css';
+import webpCss from 'gulp-webpcss';
+import autoPrefixer from 'gulp-autoprefixer';
+import groupCssMedia from 'gulp-group-css-media-queries';
 
 const sass = gulpSass(dartSass);
 
@@ -10,6 +14,18 @@ export const scss = () => {
         })
         .pipe(app.plugins.replace(/@img\//g, '../img/'))
         .pipe(sass())
+        .pipe(groupCssMedia())
+        .pipe(webpCss({
+            webpClass: '.webp',
+            noWebpClass: '.no-webp'
+        }))
+        .pipe(autoPrefixer({
+            grid: true,
+            overrideBrowserslist: ['last 3 versions'],
+            cascade: true
+        }))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
         .pipe(rename({
             extname: '.min.css'
         }))
